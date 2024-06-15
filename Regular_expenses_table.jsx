c6a0dae068baeb354bd28da0ad588252fe235510
manuo <ojhapranav1999@gmail.com>
2024-06-15 21:12:09 +0530

    First commit for the regular expenses table component

---
+++ Regular_expenses_table.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Regular_expenses_table=()=>{
+    return(
+        <>
+        <h1>THis is the main regular expense table</h1>
+        </>
+    )
+}
+
+
+
+
+// <table id='Rg_expenses_table'>
+//             <tr className='Rg_expenses_table_row'>
+//               <td>Home Rent</td>
+//               <td><button className='local_button' onClick={(e)=>{decrease_func(e.target.value)}} value={1}>-</button></td>
+//               <td id='valueofexpense_1'  >0</td>
+//               <td>Rs</td>
+//               <td><button className='local_button' onClick={(e)=>{increase_func(e.target.value)}} value={1}  >+</button></td>
+//             </tr>
+//           </table>
