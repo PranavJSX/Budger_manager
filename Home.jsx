c6b2f198e4eaa55239dbd3e9fb3b882e2096e0cc
manuo <ojhapranav1999@gmail.com>
2024-06-19 23:12:24 +0530

    added the reverse calls for props for total regular expenses

--- Home.jsx
+++ Home.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Info } from './Info'
+import { User_info } from './User_info'
 
 export const Home = () => {
+  const [Regular_total,setRegularTotal]=useState(0)
+
+  let User_obj=[{
+    Id:101,
+    Name : 'Pranav Ojha',
+    Email :  'maunojha1@gmail.com',
+    image_src : 'public/DSC04831 (2).jpg'
+  },]
+
+  const set_total=(val)=>{
+    setRegularTotal(val)
+  }
+
+  const user_id=101
+  console.log(User_obj)
   return (
     <div>Hi User! , Welcome to your personal Budget Manager.
         <div><span><br/></span>
-            <section className='section1'><Info/></section>
+            <section className='section1' style={{minHeight:'620px'}}><Info get_total={set_total}/></section>
+            <section className='section2'><User_info userobj={User_obj} current_user_id={user_id} total={Regular_total}/></section>
+            <footer>This is the footer</footer>
         </div>
     </div>
   )
 }
+
+
+
+
